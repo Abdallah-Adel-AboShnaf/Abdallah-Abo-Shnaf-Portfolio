e253f76a2c7765022c9301c493c6641c10fd470d
Abdallah Adel Abdallah Mahmoud <154610653+Abdallah-Adel-AboShnaf@users.noreply.github.com>
2025-06-17 10:41:44 +0000

    v24

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -47,7 +47,7 @@ const About = () => {
             <Card className="p-8 bg-white/80 backdrop-blur-sm border-slate-200">
               <h2 className="text-2xl font-semibold text-slate-800 mb-4">My Story</h2>
               <div className="prose prose-slate max-w-none space-y-4 text-slate-600">
-                <ul className="list-disc pl-6 text-sky-700 leading-relaxed space-y-4"></ul>
+                <ul className="list-disc pl-6 text-sky-700 leading-relaxed space-y-4">
                   <li>
                     A passionate Data Scientist with a strong foundation in data analysis, machine learning, and statistical modeling. I am currently pursuing a degree in Data Science and have been awarded competitive scholarships from both DEPI and ALX Africa.
                   </li>
@@ -59,6 +59,7 @@ const About = () => {
                   <li>
                     Driven by curiosity and a dedication to solving real-world problems, I aim to make meaningful contributions in the world of Data Science.
                   </li>
+                </ul>
               </div>
             </Card>
 
